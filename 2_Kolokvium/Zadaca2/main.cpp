e80a0edc890c86421cea700b8d64e265cb5f9223
Borjan Najdoski <najdoskib@gmail.com>
2024-01-22 05:42:34 +0100

    Kreiranje na zadaca 2 od vtor kolokvium

---
+++ 2_Kolokvium/Zadaca2/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+// vashiot kod ovde
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    Vozac **v = new Vozac*[n];
+    char ime[100];
+    int vozrast;
+    int trki;
+    bool vet;
+    for(int i = 0; i < n; ++i) {
+        cin >> ime >> vozrast >> trki >> vet;
+        if(i < x) {
+            float cena_avto;
+            cin >> cena_avto;
+            v[i] = new Avtomobilist(ime, vozrast, trki, vet, cena_avto);
+        } else {
+            int mokjnost;
+            cin >> mokjnost;
+            v[i] = new Motociklist(ime, vozrast, trki, vet, mokjnost);
+        }
+    }
+    cout << "=== DANOK ===" << endl;
+    for(int i = 0; i < n; ++i) {
+        cout << *v[i];
+        cout << v[i]->danok() << endl;
+    }
+    cin >> ime >> vozrast >> trki >> vet;
+    int mokjnost;
+    cin >> mokjnost;
+    Vozac *vx = new Motociklist(ime, vozrast, trki, vet, mokjnost);
+    cout << "=== VOZAC X ===" << endl;
+    cout << *vx;
+    cout << "=== SO ISTA ZARABOTUVACKA KAKO VOZAC X ===" << endl;
+    cout << soIstaZarabotuvachka(v, n, vx);
+    for(int i = 0; i < n; ++i) {
+        delete v[i];
+    }
+    delete [] v;
+    delete vx;
+    return 0;
+}
