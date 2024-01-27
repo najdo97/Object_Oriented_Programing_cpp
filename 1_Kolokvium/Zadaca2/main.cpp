913b3ea8dcbc24f220f0bdb7c0f782edf966235b
Borjan Najdoski <najdoskib@gmail.com>
2024-01-27 19:16:03 +0100

    resena 2 zadaca od 1 kolokvium

--- 1_Kolokvium/Zadaca2/main.cpp
+++ 1_Kolokvium/Zadaca2/main.cpp
@@ -6,18 +6,26 @@ class List {
 private:
     int *niza_broevi;
     int brojac;
+
+
 public:
+
+    int getBrojac() {
+        return brojac;
+    }
+
     //constructor
-    List() {};
+    List() {
+        niza_broevi = nullptr;
+        brojac = 0;
+    };
 
     List(int *niza_broevi, int brojac) {
 
-        cout<<"constructor na List"<<endl;
         this->niza_broevi = new int[brojac];
         for (int i = 0; i < brojac; i++) {
             this->niza_broevi[i] = niza_broevi[i];
         }
-
         this->brojac = brojac;
     }
 
@@ -37,12 +45,15 @@ public:
     ~List() {
         delete[]niza_broevi;
     }
+
     //operator =
 
-    List &operator=(const List &other) {
-        if (this !=
-            &other) { // moze i bez ova, ama dobra praktika e da se proveri self assigment, ako vrednostite se isti na dvata objekta, nema potreba da se aktiviraat operaciite vo blokot naredbi
+    List &operator=(const List& other) {
+        if (this != &other) {
+            delete[] this->niza_broevi;     //todo - tuka javuva bug
+
             this->niza_broevi = new int[other.brojac];
+
             for (int i = 0; i < other.brojac; i++) {
                 this->niza_broevi[i] = other.niza_broevi[i];
             }
@@ -51,36 +62,29 @@ public:
         return *this;
     }
 
-//todo
     void pecati() {
 
-        cout << this->brojac <<": ";
+        cout << this->brojac << ": ";
         for (int i = 0; i < this->brojac; i++) {
-            cout << this->niza_broevi[i]<<" ";
+            cout << this->niza_broevi[i] << " ";
         }
-        cout << sum() << " " << average();
-
-        //[број на елементи во листата]: x1 x2 .. xn sum: [сума] average: [просек]
+        cout <<"sum: " <<sum() << " " <<"average: " << average()<< endl;
     }
 
-//todo
-    int sum() {
+   int sum() {
 
         int sum = 0;
 
         for (int i = 0; i < this->brojac; i++) {
             sum = sum + this->niza_broevi[i];
         }
-        //vrakja suma na elementite vo listata
         return sum;
     }
 
-//todo
     double average() {
         double average = 0.0;
+        average = (double)sum() / this->brojac;
 
-        average = sum() / this->brojac;
-        //prosekot na broevite vo listata
         return average;
     }
 };
@@ -90,19 +94,19 @@ class ListContainer {
 private:
     List *niza_listi;
     int broj_na_listi;
-    int obidi;  //se inicijalizira na 0 vo konstruktorot
+    int obidi;
+
 
 public:
 
     //default construcotr - obidi se inicijalizira na 0
     ListContainer() {
 
-        // todo - how do i iterate and initilize the default values of a dynamic array ? - sakam site clenovi da se inicijalizirani na 0
-
-        this->broj_na_listi=0;
+        this->niza_listi = new List[25];
+        this->broj_na_listi = 0;
         this->obidi = 0;
 
-        }
+    }
 
     //parametarized constructor
     ListContainer(List *niza_listi, int broj_na_listi, int obidi) {
@@ -121,6 +125,7 @@ public:
 
     ListContainer(const ListContainer &copyListContainer) {
 
+
         this->niza_listi = new List[copyListContainer.broj_na_listi];   // inicijaliziranje i rezerviranje mem lokacija
 
         for (int i = 0; i < copyListContainer.broj_na_listi; i++) {
@@ -138,56 +143,45 @@ public:
 
 
     //operator =
+
     ListContainer &operator=(const ListContainer &other) {
 
         if (this != &other) {
+            delete[]niza_listi;         //todo - tuka ke javuva bug
+            this->niza_listi = new List[25];
 
-            this->niza_listi = new List[other.broj_na_listi];
             for (int i = 0; i < other.broj_na_listi; i++) {
                 this->niza_listi[i] = other.niza_listi[i];
             }
             this->broj_na_listi = other.broj_na_listi;
             this->obidi = other.obidi;
-
         }
         return *this; // vrakja pokazuvac kon samiot ovoj objekt
     }
 
-
-/*todo
-    метод што ќе ги печати информациите за контејнерот во форматот:
-    List number: [реден број на листата]
-    List info: [испечатени информации за листата со методот List::pecati()] \n sum: [сума] average: [просек]
-    доколку контејнерот е празен се печати само порака The list is empty.
-*/
     void print() {
-        if (this->broj_na_listi==0){
-            cout<<"The list is empty"<<endl;
-        }
-        else {
+        if (this->broj_na_listi <= 0) {
+            cout << "The list is empty" << endl;
+        } else {
 
             for (int i = 0; i < this->broj_na_listi; i++) {
-                cout<<"List number: " << this->broj_na_listi ;
-            }
-
-            for (int i = 0; i < this->broj_na_listi; i++) {
-                cout<<"List info: " ;
+                cout << "List number: " << i+1 << " List info: ";
                 this->niza_listi[i].pecati();
             }
+            cout << "Sum: " << sum() << " Average: " << average() << "\n";
+            cout << "Successful attempts: " << this->broj_na_listi << " Failed attempts: " << (obidi - broj_na_listi) << "\n";
 
         }
     }
 
-//todo - void addNewList(List l) метод со којшто се додава листа во контејнерот
-//Напомена: една листа се додава во контејнерот ако и само ако има различна сума од сите листи што се веќе додадени во контејнерот
+
 
     void addNewList(List l) {
-        cout<<" vleze vo addNewList"<<endl;
-        this->obidi++;
+
+        obidi++;
         bool check = false;
 
         for (int i = 0; i < this->broj_na_listi; i++) {
-            cout<<" vleze vo for Loop"<<endl;
 
             if (this->niza_listi[i].sum() == l.sum()) {
                 check = true;
@@ -196,31 +190,15 @@ public:
         }
         if (check == false) //if sucsessfull, zanci ima vekje ima lista so ista suma
         {
-            cout<<" vleze vo if uslov, zanci ke probame da ja dodademe novata lista"<<endl;
-            cout<<this->broj_na_listi<<endl;
-            cout<<&broj_na_listi<<endl;
-
-            /*
-             Za da mozeme da dodademe nov clen na nizata, morame starata niza da ja kopirame privremeno vo druga niza koja ke
-             ima novoalocirana memorija za +1 clen. Otposle treba site vrednosti da gi prefrlime vo novata niza, i na kraj
-             da go dodademe noviot clen.
-
-             */
-            //todo - alociraj memorija na nova lista taka shto ke ima +1 clen novata lista
-            //todo - kopiraj gi clenovite od nova stara lista vo nova lista
-            //todo - dodaj go noviot clen na listata so = operator, toj sho go prepokri pogore !
-
-
-            // this->broj_na_listi++;                      // ne sum siguren deka ova e dovolno, bidejki ovdeka nikade ne go inicijalizirame ovoj nov clen
-            // this-> niza_listi=new List[this->broj_na_listi];    // ova e vozmozno bidejki go overloadnavme = operatorot
-            // this->niza_listi[this->broj_na_listi]=l;    // ova e vozmozno bidejki go overloadnavme = operatorot
+           this->niza_listi[this->broj_na_listi++] = l;
         }
 
     }
 
+
     int sum() {
 
-        int suma=0;
+        int suma = 0;
         for (int i = 0; i < this->broj_na_listi; i++) {
             suma = suma + this->niza_listi[i].sum();
         }
@@ -229,11 +207,12 @@ public:
 
 
     double average() {
-        double prosek=0;
+        double prosek = 0;
         for (int i = 0; i < this->broj_na_listi; i++) {
-            prosek = prosek + this->niza_listi[i].average();
+            prosek = prosek + this->niza_listi[i].getBrojac();
         }
-        return prosek;
+
+        return (double)sum()/prosek;
     }
 };
 
