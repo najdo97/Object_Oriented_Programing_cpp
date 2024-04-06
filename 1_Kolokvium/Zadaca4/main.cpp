4494f96eb25fa0a2d2fbd2e5b521ddb6eca39fb8
Borjan Najdoski <najdoskib@gmail.com>
2024-04-06 16:48:34 +0200

    Kolok1-Zad4

--- 1_Kolokvium/Zadaca4/main.cpp
+++ 1_Kolokvium/Zadaca4/main.cpp
@@ -1,5 +1,48 @@
-#include <iostream>
 
-int main() {
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Patnik{
+
+private:
+    char ime[100];
+    int tip_vagon;
+    bool velosiped;
+public:
+
+
+};
+
+class Voz{
+
+private:
+
+public:
+
+};
+
+
+int main()
+{
+    Patnik p;
+    char ime[100], destinacija[100];
+    int n;
+    bool velosiped;
+    int klasa;
+    int maxv;
+    cin >> destinacija >> maxv;
+    cin >> n;
+    Voz v(destinacija, maxv);
+    //cout<<v<<endl;
+    for (int i = 0; i < n; i++){
+        cin >> ime >> klasa >> velosiped;
+        Patnik p(ime, klasa, velosiped);
+        //cout<<p<<endl;
+        v += p;
+    }
+    cout << v;
+    v.patniciNemaMesto();
+
     return 0;
 }
