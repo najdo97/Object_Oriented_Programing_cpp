07d4032c94c8fa97852a53d9b2663e85daeb1398
Borjan Najdoski <najdoskib@gmail.com>
2024-04-14 14:59:58 +0200

    Preresuvanje na  Kolok1_Zad13

--- 1_Kolokvium/Zadaca13/main.cpp
+++ 1_Kolokvium/Zadaca13/main.cpp
@@ -3,234 +3,155 @@
 
 #include<string.h>
 
-
 using namespace std;
 
 class Zichara {
+
 private:
     char *mesto;
     int cena;
 
 public:
-    char *getMesto() const {
-        return this->mesto;
-    }
-
-    void setMesto(char *mesto) {
-        this->mesto = mesto;
-    }
-
-    int getCena() const {
-        return this->cena;
-    }
-
-    void setCena(int cena) {
-        this->cena = cena;
-    }
-
-
     Zichara() {
-        strcpy(this->mesto, "nan");
+        this->mesto = nullptr;
         this->cena = 0;
-    }
+    };
 
     Zichara(char *mesto, int cena) {
         this->mesto = new char[strlen(mesto) + 1];
         strcpy(this->mesto, mesto);
 
         this->cena = cena;
-    }
+    };
 
     Zichara(const Zichara &z) {
         this->mesto = new char[strlen(z.mesto) + 1];
         strcpy(this->mesto, z.mesto);
 
         this->cena = z.cena;
-    }
+    };
 
-    virtual ~Zichara() {
-        delete[]mesto;
+    ~Zichara() {
+        delete[]this->mesto;
+    };
+
+    int getCena() {
+        return this->cena;
     }
 
     Zichara &operator=(const Zichara &z) {
+
         delete[]this->mesto;
 
         this->mesto = new char[strlen(z.mesto) + 1];
         strcpy(this->mesto, z.mesto);
 
         this->cena = z.cena;
-
         return *this;
-    }
-
-
+    };
 };
 
 class PlaninarskiDom {
 
 private:
     char ime[15];
-    int cena_sezona[2]; //0-Letna cena ; 1-Zimska cena
-    char kvalitet;      // ova moze i so enum da e izvede
-    bool zicara;
-    Zichara *planinskiDom_zicara;
+    int sezonska_cena[2];
+    char klasa_na_dom;
+    bool imaZichara;
+    Zichara *zichara;
 
 public:
-    void setIme(char *Ime) {
-        strcpy(this->ime, ime);
-    }
-
-    void setCena_Sezona(int *cena_sezona) {
-        for (int i = 0; i < 2; i++) {
-            this->cena_sezona[i] = cena_sezona[i];
-        }
-    }
-
-    void setKvalitet(char kvalitet) {
-        this->kvalitet = kvalitet;
-    }
-
-    void setZichara(Zichara z) {
-
-        this->planinskiDom_zicara[0] = z;
-        this->zicara = true;
-    }
-
-    void setPlaninskiDom_zicara(Zichara *planinskiDomZicara) {
-        this->planinskiDom_zicara = planinskiDomZicara;
-    }
-
-    const char *getIme() const {
-        return this->ime;
-    }
-
-    const int *getCenaSezona() const {
-        return this->cena_sezona;
-    }
-
-    char getKvalitet() const {
-        return this->kvalitet;
-    }
-
-    bool isZicara() const {
-        return this->zicara;
-    }
-
-    Zichara *getPlaninskiDomZicara() const {
-        return planinskiDom_zicara;
-    }
-
     PlaninarskiDom() {
 
-        strcpy(this->ime, "nan");
-        for (int i = 0; i < 2; i++) {
-            this->cena_sezona[i] = 0;
-        }
-        this->kvalitet = 'n';
-        this->zicara = false;
-        this->planinskiDom_zicara = nullptr;
-    }
+        strcpy(this->ime, "NaN");
 
-    PlaninarskiDom(char *ime, int *cenaSezona, char kvalitet) {
+        this->sezonska_cena[0] = 0;  // leto
+        this->sezonska_cena[1] = 0;  // zimo
+
+        this->klasa_na_dom = 'F';
+        this->imaZichara = false;
+
+        this->zichara = nullptr;
+    };
+
+    PlaninarskiDom(char *ime, int *sezonska_cena, char klasa) {
         strcpy(this->ime, ime);
-        for (int i = 0; i < 2; i++) {
-            this->cena_sezona[i] = cenaSezona[i];
-        }
-        this->kvalitet = kvalitet;
 
-        this->zicara = false;
-        this->planinskiDom_zicara = nullptr;
-    }
+        this->sezonska_cena[0] = sezonska_cena[0];  // leto
+        this->sezonska_cena[1] = sezonska_cena[1];  // zimo
+        this->klasa_na_dom = klasa;
 
-    PlaninarskiDom(char *ime, int *cenaSezona, char kvalitet, bool zicara, Zichara planinskiDomZicara) {
-        strcpy(this->ime, ime);
-        for (int i = 0; i < 2; i++) {
-            this->cena_sezona[i] = cenaSezona[i];
-        }
-        this->kvalitet = kvalitet;
-        this->zicara = zicara;
+        this->imaZichara = false;       // za ovie treba set metoda
+        this->zichara = nullptr;        // za ovie treba set metoda
 
-        this->planinskiDom_zicara = new Zichara[1];
-        this->planinskiDom_zicara[0] = planinskiDomZicara;
-    }
-
+    };
 
     PlaninarskiDom(const PlaninarskiDom &p) {
         strcpy(this->ime, p.ime);
-        for (int i = 0; i < 2; i++) {
-            this->cena_sezona[i] = p.cena_sezona[i];
-        }
-        this->kvalitet = p.kvalitet;
-        this->zicara = p.zicara;
 
+        this->sezonska_cena[0] = p.sezonska_cena[0];  // leto
+        this->sezonska_cena[1] = p.sezonska_cena[1];  // zimo
+        this->klasa_na_dom = p.klasa_na_dom;
+        this->imaZichara = p.imaZichara;
 
-        this->planinskiDom_zicara = new Zichara[1];
-        this->planinskiDom_zicara[0] = p.planinskiDom_zicara[0];
+        this->zichara = p.zichara;       //   I vaka da e napishano, bi trebalo da raboti     this->zichara[0] = p.zichara[0];
 
-    }
+    };
 
     ~PlaninarskiDom() {
-
-        delete[] this->planinskiDom_zicara;
-    }
-
-    PlaninarskiDom &operator=(const PlaninarskiDom &p) {
-
-        strcpy(this->ime, p.ime);
-        for (int i = 0; i < 2; i++) {
-            this->cena_sezona[i] = p.cena_sezona[i];
-        }
-        this->kvalitet = p.kvalitet;
-        this->zicara = p.zicara;
-
-        delete[] this->planinskiDom_zicara;
-
-        this->planinskiDom_zicara = new Zichara[1];
-        this->planinskiDom_zicara[0] = p.planinskiDom_zicara[0];
-
-        return *this;
+        delete[] this->zichara;
     };
+
+    void setZichara(Zichara z) {
+        this->imaZichara = true;
+        this->zichara = &z;
+    };
+
+    /// OVERLOAD OPERATOR = ZA OVAA KLASA !!!!!
+
 
 
     friend ostream &operator<<(ostream &os, const PlaninarskiDom &dom) {
-        if (dom.zicara == true) {
-            os << dom.ime << " klasa:" << dom.kvalitet << " so zichara" << endl;
-        } else os << dom.ime << " klasa:" << dom.kvalitet << endl;
 
+        if (dom.imaZichara) {
+            os << dom.ime << " klasa:" << dom.klasa_na_dom << " so zichara" << endl;
+        } else os << dom.ime << " klasa:" << dom.klasa_na_dom << endl;
 
         return os;
     }
 
-    bool operator<=(const char c) const {
-
-        if (this->kvalitet >= c) {
-            return false;
-        } else return true;
-    }
-
     PlaninarskiDom &operator--() {
 
-        if (this->kvalitet < 'F') {
-            this->kvalitet--;
+        if (this->klasa_na_dom < 'F') {
+            this->klasa_na_dom++;
         }
-
         return *this;
     }
 
-    void presmetajDnevenPrestoj(int den, int mesec, int dneven_prestoj_cena) {
+    const bool &operator<=(char z) {
 
-        if ((den > 31 || den < 1) || (mesec > 12 || mesec < 1)) {
-            return;
+        if (this->klasa_na_dom >= z) {
+            return true;
+        } else return false;
+    };
+
+
+    void presmetajDnevenPrestoj(int den, int mesec, int cena) {
+
+        cena = 0;
+        if (this->imaZichara) {
+            cena = cena + this->zichara->getCena();
         }
-        if(this->zicara==true)
-        {
 
+        if (mesec < 1 || mesec > 12 || den < 1 || den > 31) {
+            throw -1;
         }
 
-    }
+            cena =cena+ (mesec >= 4 && mesec <= 8) || mesec == 9 && den == 1 ? sezonska_cena[0] : sezonska_cena[1];
 
+
+    };
 };
-
 
 int main() {
 
