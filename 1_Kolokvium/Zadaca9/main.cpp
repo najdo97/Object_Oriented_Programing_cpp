64ab87e04d1dea84a6f9bf2e273b2a47dd453608
Borjan Najdoski <najdoskib@gmail.com>
2024-04-18 02:04:37 +0200

    Postavuvanje  Kolok1_Zad9

---
+++ 1_Kolokvium/Zadaca9/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+
+int main() {
+    ITStore s[100];
+    int n;
+    cin >> n; //broj na IT prodavnici
+
+    //vnesuvanje na prodavnicite edna po edna, zaedno so raspolozlivite laptopvi vo niv
+
+
+
+    //pecatenje na site prodavnici
+
+
+
+
+    //povik na glavnata metoda
+
+    return 0;
+}
