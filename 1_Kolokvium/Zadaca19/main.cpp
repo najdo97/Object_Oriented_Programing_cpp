2c458e10bc7d074bbb51b39377e878602677634a
Borjan Najdoski <najdoskib@gmail.com>
2024-04-19 03:53:32 +0200

    Postavena zadaca Kolok1_Zad19

---
+++ 1_Kolokvium/Zadaca19/main.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+// vashiot kod ovde
+
+// zabraneto e menuvanje na main funkcijata
+
+int main() {
+    char name[100];
+    char ingr[100];
+    float price;
+    int discount;
+
+    int testCase;
+
+    cin >> testCase;
+    cin.get();
+    if (testCase == 1) {
+        cout << "====== TESTING IceCream CLASS ======" << endl;
+        cin.getline(name, 100);
+        cin.getline(ingr, 100);
+        cin >> price;
+        cin >> discount;
+        cout << "CONSTRUCTOR" << endl;
+        IceCream ic1(name, ingr, price);
+        ic1.setDiscount(discount);
+        cin.get();
+        cin.getline(name, 100);
+        cin.getline(ingr, 100);
+        cin >> price;
+        cin >> discount;
+        IceCream ic2(name, ingr, price);
+        ic2.setDiscount(discount);
+        cout << "OPERATOR <<" << endl;
+        cout << ic1 << endl;
+        cout << ic2 << endl;
+        cout << "OPERATOR ++" << endl;
+        ++ic1;
+        cout << ic1 << endl;
+        cout << "OPERATOR +" << endl;
+        IceCream ic3 = ic2 + "chocolate";
+        cout << ic3 << endl;
+    } else if (testCase == 2) {
+        cout << "====== TESTING IceCream CONSTRUCTORS ======" << endl;
+        cin.getline(name, 100);
+        cin.getline(ingr, 100);
+        cin >> price;
+        cout << "CONSTRUCTOR" << endl;
+        IceCream ic1(name, ingr, price);
+        cout << ic1 << endl;
+        cout << "COPY CONSTRUCTOR" << endl;
+        IceCream ic2(ic1);
+        cin.get();
+        cin.getline(name, 100);
+        ic2.setName(name);
+        cout << ic1 << endl;
+        cout << ic2 << endl;
+        cout << "OPERATOR =" << endl;
+        ic1 = ic2;
+        cin.getline(name, 100);
+        ic2.setName(name);
+        cout << ic1 << endl;
+        cout << ic2 << endl;
+
+        cin >> discount;
+        ic1.setDiscount(discount);
+
+
+    } else if (testCase == 3) {
+        cout << "====== TESTING IceCreamShop ======" << endl;
+        char icsName[50];
+        cin.getline(icsName, 100);
+        cout << "CONSTRUCTOR" << endl;
+        IceCreamShop ics(icsName);
+        int n;
+        cin >> n;
+        cout << "OPERATOR +=" << endl;
+        for (int i = 0; i < n; ++i) {
+            cin.get();
+            cin.getline(name, 100);
+            cin.getline(ingr, 100);
+            cin >> price;
+            IceCream ic(name, ingr, price);
+            ics += ic;
+        }
+        cout << ics;
+    } else if (testCase == 4) {
+        cout << "====== TESTING IceCreamShop CONSTRUCTORS ======" << endl;
+        char icsName[50];
+        cin.getline(icsName, 100);
+        IceCreamShop ics(icsName);
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; ++i) {
+            cin.get();
+            cin.getline(name, 100);
+            cin.getline(ingr, 100);
+            cin >> price;
+            IceCream ic(name, ingr, price);
+            ics += ic;
+        }
+        IceCream x("FINKI fruits", "strawberry ice cream, raspberry ice cream, blueberry ice cream", 60);
+        IceCreamShop icp = ics;
+        ics += x;
+        cout << ics << endl;
+        cout << icp << endl;
+    }
+
+
+    return 0;
+}
