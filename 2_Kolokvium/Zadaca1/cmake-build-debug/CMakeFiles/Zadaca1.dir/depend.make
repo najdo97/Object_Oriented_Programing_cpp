5a9f16e70e9c00d8ac27358915f99ff10b2b1b1b
Borjan Najdoski <najdoskib@gmail.com>
2024-04-24 23:58:25 +0200

    Polu resena Kolok2_Zad1

---
+++ 2_Kolokvium/Zadaca1/cmake-build-debug/CMakeFiles/Zadaca1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Zadaca1.
+# This may be replaced when dependencies are built.
--- 2_Kolokvium/Zadaca1/main.cpp
+++ 2_Kolokvium/Zadaca1/main.cpp
@@ -1,28 +1,159 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+class Book {
+private:
+    char ISBN[20];
+    char naslov[50];
+    char avtor[30];
+    float osnovna_cena;
+
+public:
+
+    Book() {
+        strcpy(this->ISBN, "NaN");
+        strcpy(this->naslov, "NaN");
+        strcpy(this->avtor, "NaN");
+
+        this->osnovna_cena = 0.0;
+    }
+
+    Book(char *ISBN, char *naslov, char *avtor, float osnovna_cena) {
+        strcpy(this->ISBN, ISBN);
+        strcpy(this->naslov, naslov);
+        strcpy(this->avtor, avtor);
+
+        this->osnovna_cena = osnovna_cena;
+    }
+
+    Book(const Book &b) {
+        strcpy(this->ISBN, b.ISBN);
+        strcpy(this->naslov, b.naslov);
+        strcpy(this->avtor, b.avtor);
+
+        this->osnovna_cena = b.osnovna_cena;
+    }
+
+    Book &operator=(const Book &b) {
+        strcpy(this->ISBN, b.ISBN);
+        strcpy(this->naslov, b.naslov);
+        strcpy(this->avtor, b.avtor);
+
+        this->osnovna_cena = b.osnovna_cena;
+
+        return *this;
+    }
+
+    void setISBN(char *isbn) {
+        strcpy(this->ISBN, isbn);
+    }
+
+    void setNaslov(char *naslov) {
+        strcpy(this->naslov, naslov);
+    }
+
+    void setAvtor(char *avtor) {
+        strcpy(this->avtor, avtor);
+    }
+
+    void setOsnovnaCena(float osnovna_cena) {
+        this->osnovna_cena = osnovna_cena;
+    }
 
 
+    const char *getIsbn() const {
+        return ISBN;
+    }
 
-int main(){
+    const char *getNaslov() const {
+        return naslov;
+    }
+
+    const char *getAvtor() const {
+        return avtor;
+    }
+
+    float getOsnovnaCena() const {
+        return osnovna_cena;
+    }
+
+
+    virtual void bookPrice() = 0;
+
+    virtual bool operator>(const Book &b) {  //neznam dali vaka se deklarira ova
+        return false;
+    };
+
+    friend ostream &operator<<(ostream &os, const Book &book) {  // neznam dali vaka se deklarira ova
+        os << book.ISBN << " " << book.naslov << " " << book.avtor << " " << book.osnovna_cena;
+        return os;
+    }
+};
+
+class OnlineBook : public Book {
+private:
+    char *url;
+    int golemina;
+
+public:
+    OnlineBook() {
+        this->url = nullptr;
+        this->golemina = 0;
+    }
+
+    OnlineBook(char *ISBN, char *naslov, char *avtor, float osnovna_cena, char *url, int golemina) : Book(ISBN, naslov,
+                                                                                                          avtor,
+                                                                                                          osnovna_cena) {
+
+        this->url = new char[strlen(url) + 1];
+        strcpy(this->url, url);
+        this->golemina = golemina;
+    }
+
+    OnlineBook(const OnlineBook &o) : Book (o) {
+
+        this->url = new char[strlen(o.url) + 1];
+        strcpy(this->url, o.url);
+        this->golemina = o.golemina;
+    }
+
+    ~OnlineBook() {
+        delete[] this->url;
+    }
+
+
+};
+
+class PrintBook : public Book {
+private:
+
+    float tezina;
+    bool naLager;
+
+public:
+
+};
+
+int main() {
 
     char isbn[20], title[50], author[30], url[100];
     int size, tip;
     float price, weight;
     bool inStock;
-    Book  **books;
+    Book **books;
     int n;
 
     int testCase;
     cin >> testCase;
 
-    if (testCase == 1){
+    if (testCase == 1) {
         cout << "====== Testing OnlineBook class ======" << endl;
         cin >> n;
         books = new Book *[n];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             cin >> isbn;
             cin.get();
             cin.getline(title, 50);
@@ -42,7 +173,7 @@ int main(){
         else
             cout << *books[1];
     }
-    if (testCase == 2){
+    if (testCase == 2) {
         cout << "====== Testing OnlineBook CONSTRUCTORS ======" << endl;
         cin >> isbn;
         cin.get();
@@ -67,12 +198,12 @@ int main(){
         cout << ob1 << endl;
         cout << ob2 << endl;
     }
-    if (testCase == 3){
+    if (testCase == 3) {
         cout << "====== Testing PrintBook class ======" << endl;
         cin >> n;
         books = new Book *[n];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             cin >> isbn;
             cin.get();
             cin.getline(title, 50);
@@ -92,12 +223,12 @@ int main(){
         else
             cout << *books[1];
     }
-    if (testCase == 4){
+    if (testCase == 4) {
         cout << "====== Testing method mostExpensiveBook() ======" << endl;
         cin >> n;
         books = new Book *[n];
 
-        for (int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
 
             cin >> tip >> isbn;
             cin.get();
@@ -111,8 +242,7 @@ int main(){
 
                 books[i] = new OnlineBook(isbn, title, author, price, url, size);
 
-            }
-            else {
+            } else {
                 cin >> weight;
                 cin >> inStock;
 
@@ -123,7 +253,7 @@ int main(){
         mostExpensiveBook(books, n);
     }
 
-    for (int i = 0; i<n; i++) delete books[i];
+    for (int i = 0; i < n; i++) delete books[i];
     delete[] books;
     return 0;
 }
