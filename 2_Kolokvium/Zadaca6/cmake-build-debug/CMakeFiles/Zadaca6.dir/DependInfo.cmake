aa7f0f1ce03c638cdb8d7d293aad3a989c042183
Borjan Najdoski <najdoskib@gmail.com>
2024-05-15 15:16:08 +0200

    Resavanje Kolok 2 Zad 6

---
+++ 2_Kolokvium/Zadaca6/cmake-build-debug/CMakeFiles/Zadaca6.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Borjan/Desktop/Object_Oriented_Programing_cpp/2_Kolokvium/Zadaca6/main.cpp" "CMakeFiles/Zadaca6.dir/main.cpp.obj" "gcc" "CMakeFiles/Zadaca6.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- 2_Kolokvium/Zadaca6/main.cpp
+++ 2_Kolokvium/Zadaca6/main.cpp
@@ -23,9 +23,82 @@ private:
     bool kupena_na_rasprodazba;
 
 public:
+    Game() {
+        strcpy(this->ime, "Nan");
+        this->cena = 0;
+        this->kupena_na_rasprodazba = false;
+    };
+
+    Game(char *ime, float cena, bool kupena_na_rasprodazba) {
+        strcpy(this->ime, ime);
+        this->cena = cena;
+        this->kupena_na_rasprodazba = kupena_na_rasprodazba;
+    };
+
+    Game(const Game &g) {
+        strcpy(this->ime, g.ime);
+        this->cena = g.cena;
+        this->kupena_na_rasprodazba = g.kupena_na_rasprodazba;
+    };
+
+    Game &operator=(const Game &g) {
+        strcpy(this->ime, g.ime);
+        this->cena = g.cena;
+        this->kupena_na_rasprodazba = g.kupena_na_rasprodazba;
+
+        return *this;
+    };
+
+    const char *getIme() const {
+        return ime;
+    }
+
+    float getCena() const {
+        return cena;
+    }
+
+    bool isKupenaNaRasprodazba() const {
+        return kupena_na_rasprodazba;
+    }
+
+    void setIme(char *ime) {
+        strcpy(this->ime, ime);
+    };
+
+    void setCena(float cena) {
+        Game::cena = cena;
+    }
+
+    void setKupenaNaRasprodazba(bool kupenaNaRasprodazba) {
+        kupena_na_rasprodazba = kupenaNaRasprodazba;
+    }
+
+    /*
+        За класите Game и SubscriptionGame да се преоптоварат операторите за печатење (<<) и читање (>>).
+        Да се дефинира и операторот == кој ќе споредува игри според нивното име.
+    */
+
+    friend ostream &operator<<(ostream &os, const Game &game) {
+        os << game.ime << " " << game.cena << " " << game.kupena_na_rasprodazba << endl;
+
+        return os;
+    }
+
+    friend istream &operator>>(istream &is, Game &game) {
+
+        is >> game.ime >> game.cena >> game.kupena_na_rasprodazba;
+
+        return is;
+    }
+
+    bool operator==(const Game &g) {
+
+        if (strcmp(this->ime, g.ime) == 0) {
+            return true;
+        } else return false;
+    }
 
 };
-
 
 class SubscriptionGame : public Game {
 
@@ -35,7 +108,58 @@ private:
     int godina_kupuvanje;
 
 public:
+    SubscriptionGame() : Game() {
+        this->pretplata = 0;
+        this->mesec_kupuvanje = 0;
+        this->godina_kupuvanje = 0;
+    }
 
+    SubscriptionGame(char *ime, float cena, bool kupena_na_rasprodazba, float pretplata, int mesec_kupuvanje,
+                     int godina_kupuvanje) : Game(ime, cena, kupena_na_rasprodazba) {
+        this->pretplata = pretplata;
+        this->mesec_kupuvanje = mesec_kupuvanje;
+        this->godina_kupuvanje = godina_kupuvanje;
+    }
+
+    SubscriptionGame(const SubscriptionGame &s) : Game(s) {
+        this->pretplata = s.pretplata;
+        this->mesec_kupuvanje = s.mesec_kupuvanje;
+        this->godina_kupuvanje = s.godina_kupuvanje;
+    }
+
+    SubscriptionGame &operator=(const SubscriptionGame &s) {
+        Game::operator=(s);
+
+        this->pretplata = s.pretplata;
+        this->mesec_kupuvanje = s.mesec_kupuvanje;
+        this->godina_kupuvanje = s.godina_kupuvanje;
+
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &os, const SubscriptionGame &game) {
+
+
+        os << dynamic_cast<const Game &>(game) << " " <<
+           game.pretplata << " " <<
+           game.mesec_kupuvanje << " " <<
+           game.godina_kupuvanje << " " << endl;
+
+        return os;
+    }
+
+    friend istream &operator<<(istream &is, const SubscriptionGame &game) {
+
+        return is;
+    }
+
+    // operator==  bi trebalo da moze da se iskorist od base class, imeto samo tamu se zacucuca
+    // za da se iskoristi meze ke treba da se upcastne so dynamic_cast<>
+
+    /*
+        За класите Game и SubscriptionGame да се преоптоварат операторите за печатење (<<) и читање (>>).
+        Да се дефинира и операторот == кој ќе споредува игри според нивното име.
+    */
 
 };
 
@@ -44,9 +168,36 @@ class User {
 private:
     char username[100];
     Game *kupeni_igri;
+    int br_igri;
 
 public:
+    User() {
+        strcpy(this->username, "nan");
+        this->kupeni_igri = nullptr;
+        this->br_igri=0;
+    }
 
+    User(char *username, Game *kupeni_igri) {
+        strcpy(this->username, username);
+
+        for (int i = 0; !(this->kupeni_igri) ; i++) {
+
+        }
+    }
+
+    User() {
+        strcpy(this->username, "nan");
+        this->kupeni_igri = nullptr;
+    }
+
+    ~User() {
+        delete[] kupeni_igri;
+    }
+
+    /*     Да се преоптовари операторот += кој ќе овозможи додавање на нова игра во колекцијата на игри.
+         Притоа ако корисникот ја има веќе купено играта, потребно е да се креира исклучок од типот ExistingGame.
+         Класата за имплементација на исклучоци потребно е има соодветен конструктор и метода message за печатење на порака на екран.
+*/
 
 };
 
@@ -89,7 +240,8 @@ int main() {
         cin >> sub_game_monthly_fee;
         cin >> sub_game_month >> sub_game_year;
 
-        SubscriptionGame sg(game_name, game_price, game_on_sale, sub_game_monthly_fee, sub_game_month, sub_game_year);
+        SubscriptionGame sg(game_name, game_price, game_on_sale, sub_game_monthly_fee, sub_game_month,
+                            sub_game_year);
         cout << sg;
     } else if (test_case_num == 3) {
         cout << "Testing operator>> for Game" << std::endl;
@@ -137,7 +289,8 @@ int main() {
 
                     cin >> sub_game_monthly_fee;
                     cin >> sub_game_month >> sub_game_year;
-                    g = new SubscriptionGame(game_name, game_price, game_on_sale, sub_game_monthly_fee, sub_game_month,
+                    g = new SubscriptionGame(game_name, game_price, game_on_sale, sub_game_monthly_fee,
+                                             sub_game_month,
                                              sub_game_year);
                 }
 
