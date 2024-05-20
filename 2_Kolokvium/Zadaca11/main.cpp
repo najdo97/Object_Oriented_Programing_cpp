d3c4e334ef8050f157f306bdad8a16b612af5149
Borjan Najdoski <najdoskib@gmail.com>
2024-05-20 13:22:47 +0200

    Kreiranje Kolok2-Zad11

---
+++ 2_Kolokvium/Zadaca11/main.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+//место за вашиот код
+
+
+//citanje na delo
+Delo readDelo() {
+    char ime[50];
+    int godina;
+    char zemja[50];
+    cin >> ime >> godina >> zemja;
+    return Delo(ime, godina, zemja);
+}
+
+//citanje na pretstava
+Pretstava *readPretstava() {
+    int tip; //0 za Balet , 1 za Opera
+    cin >> tip;
+    Delo d = readDelo();
+    int brojProdadeni;
+    char data[15];
+    cin >> brojProdadeni >> data;
+    if (tip == 0) return new Balet(d, brojProdadeni, data);
+    else return new Opera(d, brojProdadeni, data);
+}
+
+int main() {
+    int test_case;
+    cin >> test_case;
+
+    switch (test_case) {
+        case 1:
+            //Testiranje na klasite Opera i Balet
+        {
+            cout << "======TEST CASE 1=======" << endl;
+            Pretstava *p1 = readPretstava();
+            cout << p1->getDelo().getIme() << endl;
+            Pretstava *p2 = readPretstava();
+            cout << p2->getDelo().getIme() << endl;
+        }
+            break;
+
+        case 2:
+            //Testiranje na  klasite Opera i Balet so cena
+        {
+            cout << "======TEST CASE 2=======" << endl;
+            Pretstava *p1 = readPretstava();
+            cout << p1->cena() << endl;
+            Pretstava *p2 = readPretstava();
+            cout << p2->cena() << endl;
+        }
+            break;
+
+        case 3:
+            //Testiranje na operator ==
+        {
+            cout << "======TEST CASE 3=======" << endl;
+            Delo f1 = readDelo();
+            Delo f2 = readDelo();
+            Delo f3 = readDelo();
+
+            if (f1 == f2) cout << "Isti se" << endl; else cout << "Ne se isti" << endl;
+            if (f1 == f3) cout << "Isti se" << endl; else cout << "Ne se isti" << endl;
+
+        }
+            break;
+
+        case 4:
+            //testiranje na funkcijata prihod
+        {
+            cout << "======TEST CASE 4=======" << endl;
+            int n;
+            cin >> n;
+            Pretstava **pole = new Pretstava *[n];
+            for (int i = 0; i < n; i++) {
+                pole[i] = readPretstava();
+
+            }
+            cout << prihod(pole, n);
+        }
+            break;
+
+        case 5:
+            //testiranje na prihod so izmena na cena za 3d proekcii
+        {
+            cout << "======TEST CASE 5=======" << endl;
+            int cenaBalet;
+            cin >> cenaBalet;
+            Balet::setCenaBalet(cenaBalet);
+            int n;
+            cin >> n;
+            Pretstava **pole = new Pretstava *[n];
+            for (int i = 0; i < n; i++) {
+                pole[i] = readPretstava();
+            }
+            cout << prihod(pole, n);
+        }
+            break;
+
+        case 6:
+            //testiranje na brojPretstaviNaDelo
+        {
+            cout << "======TEST CASE 6=======" << endl;
+            int n;
+            cin >> n;
+            Pretstava **pole = new Pretstava *[n];
+            for (int i = 0; i < n; i++) {
+                pole[i] = readPretstava();
+            }
+
+            Delo f = readDelo();
+            cout << brojPretstaviNaDelo(pole, n, f);
+        }
+            break;
+
+    };
+
+
+    return 0;
+}
