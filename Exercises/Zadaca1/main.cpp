b5816f28b85db7fcda9d8bb6f2b2153e71b9f353
Borjan Najdoski <najdoskib@gmail.com>
2024-05-25 15:50:26 +0200

    Kreiranje Excercises-Zad1

---
+++ Exercises/Zadaca1/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+// YOUR CODE HERE
+
+// DO NOT CHANGE THE MAIN FUNCTION
+
+
+int main() {
+    int testCase;
+    cin >> testCase;
+    float p;
+    char name[100];
+    char country[100];
+    float price;
+    bool mainI;
+    int year;
+    char grape [20];
+    if(testCase == 1) {
+        cout << "===== TESTING CONSTRUCTORS ======" << endl;
+        cin >> p;
+        cin >> name;
+        cin >> country;
+        cin >> price;
+        cin >> mainI;
+        Beer b(p, name, country, price, mainI);
+        cout << b << endl;
+        cin >> p;
+        cin >> name;
+        cin >> country;
+        cin >> price;
+        cin >> year;
+        cin >> grape;
+        Wine w(p, name, country, price, year, grape);
+        cout << w << endl;
+
+    } else if(testCase == 2) {
+        cout << "===== TESTING LOWEST PRICE ======" << endl;
+        int n;
+        cin >> n;
+        AlcoholicDrink** ad = new AlcoholicDrink*[n];
+        for(int i = 0; i < n; ++i) {
+            cin >> p;
+            cin >> name;
+            cin >> country;
+            cin >> price;
+
+            if(i % 2 == 1){
+                cin >> mainI;
+                ad[i] = new Beer(p, name, country, price, mainI);
+            }
+            else {
+                cin >> year;
+                cin >> grape;
+                ad[i] = new Wine(p, name, country, price, year, grape);
+            }
+        }
+
+        lowestPrice(ad, n);
+        for(int i = 0; i < n; ++i) {
+            delete ad[i];
+        }
+        delete [] ad;
+    } else if(testCase == 3) {
+        cout << "===== TESTING DISCOUNT STATIC ======" << endl;
+        int n;
+        cin >> n;
+        AlcoholicDrink** ad = new AlcoholicDrink*[n];
+        for(int i = 0; i < n; ++i) {
+            cin >> p;
+            cin >> name;
+            cin >> country;
+            cin >> price;
+            if(i % 2 == 1){
+                cin >> mainI;
+                ad[i] = new Beer(p, name, country, price, mainI);
+            }
+            else {
+                cin >> year;
+                cin >> grape;
+                ad[i] = new Wine(p, name, country, price, year, grape);
+            }
+        }
+        AlcoholicDrink::total(ad, n);
+        int d;
+        cin >> d;
+        AlcoholicDrink::changeDiscount(d);
+        AlcoholicDrink::total(ad, n);
+        for(int i = 0; i < n; ++i) {
+            delete ad[i];
+        }
+        delete [] ad;
+    }
+
+}
+
