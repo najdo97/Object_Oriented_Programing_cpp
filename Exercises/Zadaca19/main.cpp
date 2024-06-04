3d5511400d1e1c169884137218ef2a7dc9117090
Borjan Najdoski <najdoskib@gmail.com>
2024-06-04 21:46:26 +0200

    Resna i debagirana EN-Zad19

--- Exercises/Zadaca19/main.cpp
+++ Exercises/Zadaca19/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <cstring>
 
 using namespace std;
 
@@ -10,6 +11,10 @@ private:
     static int processing_price;
 
 public:
+
+    int getId() const {
+        return parcel_ID;
+    }
 
     Parcel() {
         this->parcel_ID = 0;
@@ -29,13 +34,129 @@ public:
     }
 
     ~Parcel() {}
-
-    void setProcessingPrice(int pom) {
+    static void setProcessingPrice(int pom) {
         processing_price = pom;
+    }
+
+    int getProcessingPrice() {
+        return processing_price;
     }
 };
 
+class Envelope : public Parcel {
 
+private:
+    int width;
+    int highth;
+    char reciver[50];
+
+public:
+    const char *getReceiver() const {
+        return reciver;
+    }
+
+    Envelope() : Parcel() {
+        this->width = 0;
+        this->highth = 0;
+        strcpy(this->reciver, "NaN");
+    }
+
+    Envelope(int parcel_ID, char *reciver, int width, int highth) : Parcel(parcel_ID) {
+        strcpy(this->reciver, reciver);
+        this->width = width;
+        this->highth = highth;
+    }
+
+    Envelope(const Envelope &other) : Parcel(other) {
+        strcpy(this->reciver, other.reciver);
+        this->width = other.width;
+        this->highth = other.highth;
+    }
+
+    Envelope &operator=(const Envelope &other) {
+        Parcel::operator=(other);
+
+        strcpy(this->reciver, other.reciver);
+        this->width = other.width;
+        this->highth = other.highth;
+        return *this;
+    }
+
+    int price() {
+        int total = 0;
+        total = this->getProcessingPrice() + (this->highth * this->width * 0.3);
+
+        return total;
+    }
+
+};
+
+class PostOffice {
+private:
+    Envelope arr[100];
+    int n;
+public:
+    PostOffice() {
+        this->n = 0;
+    }
+
+    PostOffice(Envelope *arr, int n) {
+        for (int i = 0; i < n; i++) {
+            this->arr[i] = arr[i];
+        }
+        this->n = n;
+    }
+
+    PostOffice(const PostOffice &other) {
+        for (int i = 0; i < other.n; i++) {
+            this->arr[i] = other.arr[i];
+        }
+        this->n = other.n;
+    }
+
+    PostOffice &operator=(const PostOffice &other) {
+        for (int i = 0; i < other.n; i++) {
+            this->arr[i] = other.arr[i];
+        }
+        this->n = other.n;
+        return *this;
+    }
+
+//  that adds an envelop with the given arguments in the array of envelops. This method returns the price for sending the envelope. (5 points)
+    int sendEnvelope(int id, char *receiver, int length, int width) {
+        if (n < 100) {
+            Envelope *tmp = new Envelope(id, receiver, width, length);
+
+            this->arr[n] = *tmp;
+            this->n++;
+        }
+
+        return this->arr[this->n - 1].price();
+    }
+
+// that prints the ID of the envelops that are addressed to the passed receiver. The printing is in the following format (5 points):
+//
+//For [receiver] are envelops with ids:  [ID1] [ID2]  ...
+    void printEnvelopsTo(char *receiver) {
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (strcmp(this->arr[i].getReceiver(), receiver) == 0) {
+                idx = i;
+
+            }
+        }
+        cout <<"For " <<this->arr[idx].getReceiver() << " are envelops with ids: ";
+
+        for (int i = 0; i < n; i++) {
+            if (strcmp(this->arr[i].getReceiver(), receiver) == 0) {
+                cout  << this->arr[i].getId() <<" ";
+            }
+        }
+
+        cout<<'\n';
+    }
+
+};
 
 
 int Parcel::processing_price = 150;
--- Exercises/Zadaca20/cmake-build-debug/cmake_install.cmake
+++ Exercises/Zadaca20/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Borjan/Desktop/Object_Oriented_Programing_cpp/Exercises/Zadaca19
+# Install script for directory: C:/Users/Borjan/Desktop/Object_Oriented_Programing_cpp/Exercises/Zadaca20
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Zadaca19")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Zadaca20")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Borjan/Desktop/Object_Oriented_Programing_cpp/Exercises/Zadaca19/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Borjan/Desktop/Object_Oriented_Programing_cpp/Exercises/Zadaca20/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
